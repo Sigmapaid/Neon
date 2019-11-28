c157fe447dcdbbb7ad259efcf8ca7a5619b94f74
Łukasz Żeleźnicki <l.zeleznicki@lsas.aero>
2019-11-28 23:51:22 +0100

    Retrofit_v.2.1

--- app/src/main/java/com/example/neon/Activity/SecondActivity.java
+++ app/src/main/java/com/example/neon/Activity/SecondActivity.java
@@ -17,15 +17,8 @@ import com.example.neon.R;
 import com.example.neon.data.model.Flight;
 import com.example.neon.data.model.FlightList;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -43,8 +36,6 @@ public class SecondActivity extends AppCompatActivity {
 //        String accessToken = getIntent().getStringExtra("token");
 //        Log.i("Token: ",accessToken);
 
-//        _loadAPI_POST(accessToken);
-
 //        String json2 = new Gson().toJson(data);
 //        data = new Gson().fromJson(mResponse, FlightList.class);
 
@@ -53,11 +44,11 @@ public class SecondActivity extends AppCompatActivity {
 //        Collection<Flight> postsList = new Gson().fromJson(flightJson, collectionType);
 //        data = new ArrayList<>(postsList);
 
-//                                data = new Gson().fromJson(result, FlightList.class);
+//        data = new Gson().fromJson(result, FlightList.class);
 //        List<Flight> postsList = Arrays.asList(new Gson().fromJson(flightJson,Flight.class));
 
         String flightJson = getIntent().getStringExtra("flight_json");
-        Log.i("String po intent ", flightJson);
+//        Log.i("String po intent ", flightJson);
         data =  new Gson().fromJson(flightJson, FlightList.class);
         adapter = new FlightsAdapter(this, data);
         lvFlight.setAdapter(adapter);
@@ -84,106 +75,6 @@ public class SecondActivity extends AppCompatActivity {
         editor.clear();
         editor.commit();
         super.onBackPressed();
-    }
-
-    public void _loadAPI_POST( String token) {
-        // Volley is an HTTP library that makes networking for Android apps easier and most importantly, faster.
-        //Volley is not suitable for large download or streaming operations, since Volley holds all responses in memory during parsing.
-        // Read https://developer.android.com/training/volley/index.html
-
-        //Add New Address
-        Map<String, String> jsonPOST = new HashMap<>();
-        jsonPOST.put("KEY", "VAL");
-        jsonPOST.put("KEY1", "VAL1");
-
-        _SEND(jsonPOST,token); // Check INTERNET is ON or NOT ?
-
-    }
-
-    private void _SEND(Map<String, String> getPARAM, String token) {
-        try {
-
-//            String URL = "http://www.mocky.io/v2/5dd322e93300002a007a4026";
-            String URL = "http://10.0.2.2:3000/flight";
-            VolleyApiCAll volleyApiCAll = new VolleyApiCAll(SecondActivity.this);
-            volleyApiCAll.Volley_GET(URL, token, new VolleyApiCAll.VolleyCallback() {
-
-                @Override
-                public void onSuccessResponse(String result) {
-
-
-                    try {
-                        if (result.matches("VOLLEY_NETWORK_ERROR")) {
-                            Toast.makeText(SecondActivity.this, "NETWORK PROBLEM", Toast.LENGTH_SHORT).show();
-                        } else {
-                            try {
-//                                System.out.println("RESULT" + result);
-                                // GET JSON THROUGH result
-//                                JSONArray jArray = new JSONArray(result);
-
-                                Type collectionType = new TypeToken<Collection<Flight>>(){}.getType();
-                                Collection<Flight> postsList = new Gson().fromJson(result, collectionType);
-
-//                                data = new Gson().fromJson(result, FlightList.class);
-//                                List<Flight> postsList = Arrays.asList(new Gson().fromJson(result,Flight.class));
-                                data = new ArrayList<>(postsList);
-//                                while (hasNext())
-//                                    action.accept(next());
-//                                 data  = new Gson().fromJson(result, Flight.class);
-                                adapter = new FlightsAdapter(SecondActivity.this, data);
-                                lvFlight.setAdapter(adapter);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-//            volleyApiCAll.Volley_POST(getPARAM, URL, new VolleyApiCAll.VolleyCallback()
-//            {
-//                @Override
-//                public void onSuccessResponse(String result)
-//                {
-//
-//
-//                    try
-//                    {
-//                        if(result.matches("VOLLEY_NETWORK_ERROR"))
-//                        {
-//                            Toast.makeText(SecondActivity.this, "NETWORK PROBLEM", Toast.LENGTH_SHORT).show();
-//                        }
-//                        else
-//                        {
-//                            try
-//                            {
-//
-//
-//                                System.out.println("RESULT"+result);
-//                                // GET JSON THROUGH result
-//
-//
-//                            }
-//                            catch (Exception e)
-//                            {
-//
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    }
-//                    catch (Exception e)
-//                    {
-//
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 
